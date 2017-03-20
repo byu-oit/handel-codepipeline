9a60da40b68cea7af47ef6b57500e8fce0a2647d
David Woodruff <david_woodruff@byu.edu>
2017-03-20 13:58:50 -0600

    Add skeleton worker service

--- test/lib/handel-worker-test.js
+++
@@ -1,7 +0,0 @@
-const expect = require('chai').expect;
-
-describe('handel-worker', function() {
-    it('should do something', function() {
-        expect(true).to.be.true;
-    });
-});
---
+++ test/lib/routes/routes-test.js
@@ -0,0 +1,56 @@
+const expect = require('chai').expect;
+const routes = require('../../../lib/routes/routes');
+
+describe('routes', function() {
+    describe('index', function() {
+        it('should respond with a simple string', function(done) {
+            let req = {}
+            let res = {
+                send: function(body) {
+                    expect(body).to.equal('Handel CodePipeline Worker');
+                    done();
+                }
+            }
+            routes.index(req, res);
+        });
+    });
+
+    describe('healthcheck', function() {
+        it('should response with a simple string', function(done) {
+            let req = {}
+            let res = {
+                send: function(body) {
+                    expect(body).to.equal('Four Seasons');
+                    done();
+                }
+            }
+            routes.healthcheck(req, res);
+        });
+    });
+
+    describe('register', function() {
+        it('should allow you to register a CodePipeline to this worker', function(done) {
+            let req = {}
+            let res = {
+                send: function(body) {
+                    expect(body).to.equal('NOT IMPLEMENTED');
+                    done();
+                }
+            }
+            routes.register(req, res);
+        });
+    });
+
+    describe('streamLogFile', function() {
+        it('should allow you to view log files for a deploy', function(done) {
+            let req = {}
+            let res = {
+                send: function(body) {
+                    expect(body).to.equal('NOT IMPLEMENTED');
+                    done();
+                }
+            }
+            routes.streamLogFile(req, res);
+        });
+    });
+});
