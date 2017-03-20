a144ad905057c32b6d9a7b553eaa48a5ffb8772b
David Woodruff <david_woodruff@byu.edu>
2017-03-20 13:40:34 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Handel-CodePipeline
+Library that creates CodePipelines with Handel as the deploy action
+
+# Library Usage
+```
+# TODOD
+
+handel-codepipeline
+```
---
+++ test/lib/handel-worker-test.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+
+describe('handel-worker', function() {
+    it('should do something', function() {
+        expect(true).to.be.true;
+    });
+});
