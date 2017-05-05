d33b8a5b95546a246550602b71a2fa7ea9c7c301
David Woodruff <david_woodruff@byu.edu>
2017-05-05 15:52:15 -0600

    Add initial validation to handel-codepipeline file.
    For now it's just some simple conditional checks. We can get more
    elaborate later if needed.

--- test/lifecycle/lifecycle-test.js
+++ test/lifecycle/lifecycle-test.js
@@ -15,6 +15,184 @@ describe('lifecycle module', function() {
         sandbox.restore();
     });
 
+    describe('validatePipelineSpec', function() {
+        it('should return an error if no pipelines are specified', function() {
+            let handelFile = {};
+            let handelCodePipelineFile = {
+                version: 1,
+
+                pipelines: {}
+            }
+
+            let errors = lifecycle.validatePipelineSpec(handelFile, handelCodePipelineFile);
+            expect(errors.length).to.equal(1);
+            expect(errors[0]).to.contain("You must specify at least one or more pipelines");
+        });
+
+        it('should return an error if no phases are specified in a pipeline', function() {
+            let handelFile = {};
+            let handelCodePipelineFile = {
+                version: 1,
+
+                pipelines: {
+                    dev: {}
+                }
+            }
+
+            let errors = lifecycle.validatePipelineSpec(handelFile, handelCodePipelineFile);
+            expect(errors.length).to.equal(1);
+            expect(errors[0]).to.contain("You must specify at least one or more phases");
+        });
+
+        it('should return an error if there are fewer than 2 phases in the pipeline', function() {
+            let handelFile = {};
+            let handelCodePipelineFile = {
+                version: 1,
+
+                pipelines: {
+                    dev: {
+                        phases: []
+                    }
+                }
+            }
+
+            let errors = lifecycle.validatePipelineSpec(handelFile, handelCodePipelineFile);
+            expect(errors.length).to.equal(1);
+            expect(errors[0]).to.contain("You must specify at least two phases");
+        });
+
+        it('should return an error if the first phase is not a github phase', function() {
+            let handelFile = {};
+            let handelCodePipelineFile = {
+                version: 1,
+
+                pipelines: {
+                    dev: {
+                        phases: [
+                            {
+                                type: 'codebuild',
+                                name: 'Build'
+                            },
+                            {
+                                type: 'codebuild',
+                                name: 'Build'
+                            }
+                        ]
+                    }
+                }
+            }
+
+            let errors = lifecycle.validatePipelineSpec(handelFile, handelCodePipelineFile);
+            expect(errors.length).to.equal(1);
+            expect(errors[0]).to.contain("must be a github phase");
+        });
+
+        it('should return an error if the second phase is not a codebuild phase', function() {
+            let handelFile = {};
+            let handelCodePipelineFile = {
+                version: 1,
+
+                pipelines: {
+                    dev: {
+                        phases: [
+                            {
+                                type: 'github',
+                                name: 'Build'
+                            },
+                            {
+                                type: 'github',
+                                name: 'Build'
+                            }
+                        ]
+                    }
+                }
+            }
+
+            let errors = lifecycle.validatePipelineSpec(handelFile, handelCodePipelineFile);
+            expect(errors.length).to.equal(1);
+            expect(errors[0]).to.contain("must be a codebuild phase");
+        });
+
+        it('should return an error if any phase does not have a type field', function() {
+            let handelFile = {};
+            let handelCodePipelineFile = {
+                version: 1,
+
+                pipelines: {
+                    dev: {
+                        phases: [
+                            {
+                                type: 'github',
+                                name: 'Build'
+                            },
+                            {
+                                type: 'codebuild',
+                                name: 'Build'
+                            },
+                            {
+                                name: 'other'
+                            }
+                        ]
+                    }
+                }
+            }
+
+            let errors = lifecycle.validatePipelineSpec(handelFile, handelCodePipelineFile);
+            expect(errors.length).to.equal(1);
+            expect(errors[0]).to.contain("must specify a type");
+        });
+
+        it('should return an error if any phase does not have a name field', function() {
+            let handelFile = {};
+            let handelCodePipelineFile = {
+                version: 1,
+
+                pipelines: {
+                    dev: {
+                        phases: [
+                            {
+                                type: 'github',
+                                name: 'Build'
+                            },
+                            {
+                                type: 'codebuild'
+                            }
+                        ]
+                    }
+                }
+            }
+
+            let errors = lifecycle.validatePipelineSpec(handelFile, handelCodePipelineFile);
+            expect(errors.length).to.equal(1);
+            expect(errors[0]).to.contain("must specify a name");
+        });
+
+        it('should work if there are no errors', function() {
+            let handelFile = {};
+            let handelCodePipelineFile = {
+                version: 1,
+
+                pipelines: {
+                    dev: {
+                        phases: [
+                            {
+                                type: 'github',
+                                name: 'Source'
+                            },
+                            {
+                                type: 'codebuild',
+                                name: 'Build'
+                            }
+                        ]
+                    }
+                }
+            }
+
+            let errors = lifecycle.validatePipelineSpec(handelFile, handelCodePipelineFile);
+            expect(errors.length).to.equal(0);
+        });
+    })
+
     describe('getPhaseSecrets', function() {
         it('should prompt for secrets from each phase', function() {
             let phaseDeployers = {
