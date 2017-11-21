6618c454abd9931c5afd1b2549d5febfef02b080
David Woodruff <david_woodruff@byu.edu>
2017-11-21 16:04:26 -0700

    Convert code build calls to TS

---
+++ src/aws/codebuild-calls.ts
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2017 Brigham Young University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+import * as AWS from 'aws-sdk';
+import * as awsWrapper from './aws-wrapper';
+
+function getCodeBuildEnvVarDef(key: string, value: string): AWS.CodeBuild.EnvironmentVariable {
+    return {
+        name: key,
+        value: value
+    };
+}
+
+function createCodeBuildProject(createParams: any): AWS.CodeBuild.CreateProjectOutput {
+    const deferred: any = {};
+    deferred.promise = new Promise((resolve, reject) => {
+        deferred.resolve = resolve;
+        deferred.reject = reject;
+    });
+
+    function createProjectRec() {
+        awsWrapper.codeBuild.createProject(createParams)
+            .then(projectResponse => {
+                deferred.resolve(projectResponse);
+            })
+            .catch(err => {
+                if (err.code === 'InvalidInputException') { // Try again because the IAM role isn't available yet
+                    setTimeout(() => {
+                        createProjectRec();
+                    }, 5000);
+                }
+                else {
+                    deferred.reject(err);
+                }
+            });
+    }
+    createProjectRec();
+
+    return deferred.promise;
+}
+
+// TODO - This function takes way too many parameters!
+function getProjectParams(projectName: string, appName: string, pipelineName: string, phaseName: string, imageName: string, environmentVariables: any, accountId: string, serviceRoleArn: string, region: string, buildSpec: string)
+    : AWS.CodeBuild.CreateProjectInput {
+
+    const projectParams: AWS.CodeBuild.Types.CreateProjectInput = {
+        name: projectName,
+        description: projectName,
+        source: {
+            type: 'CODEPIPELINE'
+        },
+        artifacts: {
+            type: 'CODEPIPELINE'
+        },
+        environment: {
+            computeType: 'BUILD_GENERAL1_SMALL',
+            image: imageName,
+            type: 'LINUX_CONTAINER',
+            environmentVariables: []
+        },
+        serviceRole: serviceRoleArn,
+        tags: [
+            {
+                key: 'Name',
+                value: projectName
+            }
+        ]
+    };
+
+    // If using a custom image, set the build to use privilegedMode. Allows access to docker daemon for docker build
+    // http://docs.aws.amazon.com/codebuild/latest/APIReference/API_ProjectEnvironment.html
+    if (imageName.startsWith(accountId)) {
+        projectParams.environment.privilegedMode = true;
+    }
+
+    // Override buildspec if specified
+    if (buildSpec) {
+        projectParams.source.buildspec = buildSpec;
+    }
+
+    // Inject pre-provided environment variables
+    projectParams.environment.environmentVariables!.push(getCodeBuildEnvVarDef('AWS_ACCOUNT_ID', accountId.toString()));
+    projectParams.environment.environmentVariables!.push(getCodeBuildEnvVarDef('AWS_REGION', region));
+    projectParams.environment.environmentVariables!.push(getCodeBuildEnvVarDef('HANDEL_APP_NAME', appName));
+    projectParams.environment.environmentVariables!.push(getCodeBuildEnvVarDef('HANDEL_PIPELINE_NAME', pipelineName));
+    projectParams.environment.environmentVariables!.push(getCodeBuildEnvVarDef('HANDEL_PHASE_NAME', phaseName));
+
+    // Add environment variables for codebuild project
+    for (const envKey of Object.keys(environmentVariables)) {
+        projectParams.environment.environmentVariables!.push(
+            getCodeBuildEnvVarDef(envKey, environmentVariables[envKey])
+        );
+    }
+
+    return projectParams;
+}
+
+// TODO - This function takes way too many parameters!
+export async function createProject(projectName: string, appName: string, pipelineName: string, phaseName: string, imageName: string, environmentVariables: any, accountId: string, serviceRoleArn: string, region: string, buildSpec: string)
+    : Promise<AWS.CodeBuild.Project | undefined> {
+    const projectParams = getProjectParams(projectName, appName, pipelineName,
+        phaseName, imageName, environmentVariables,
+        accountId, serviceRoleArn, region, buildSpec);
+
+    const createResponse = await createCodeBuildProject(projectParams);
+    return createResponse.project;
+}
+
+// TODO - This function takes way too many parameters!
+export async function updateProject(projectName: string, appName: string, pipelineName: string, phaseName: string, imageName: string, environmentVariables: any, accountId: string, serviceRoleArn: string, region: string, buildSpec: string)
+    : Promise<AWS.CodeBuild.Project | undefined> {
+    const projectParams = getProjectParams(projectName, appName, pipelineName,
+        phaseName, imageName, environmentVariables,
+        accountId, serviceRoleArn, region, buildSpec);
+
+    const updateResponse = await awsWrapper.codeBuild.updateProject(projectParams);
+    return updateResponse.project;
+}
+
+export async function getProject(projectName: string): Promise<AWS.CodeBuild.Project | null> {
+    const getParams = {
+        names: [
+            projectName
+        ]
+    };
+    const getResults = await awsWrapper.codeBuild.batchGetProjects(getParams);
+    const projects = getResults.projects;
+    if (projects && projects.length !== 0) {
+        return projects[0];
+    }
+    else {
+        return null; // No project
+    }
+}
+
+export function deleteProject(projectName: string): Promise<boolean> {
+    const deleteParams = {
+        name: projectName
+    };
+    return awsWrapper.codeBuild.deleteProject(deleteParams)
+        .then(() => {
+            return true;
+        });
+}
---
+++ test/aws/codebuild-calls-test.ts
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2017 Brigham Young University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import * as awsWrapper from '../../src/aws/aws-wrapper';
+import * as codeBuildCalls from '../../src/aws/codebuild-calls';
+
+describe('codebuild calls module', () => {
+    let sandbox: sinon.SinonSandbox;
+
+    beforeEach(() => {
+        sandbox = sinon.sandbox.create();
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    describe('createProject', () => {
+        it('should create the project', async () => {
+            const projectName = 'FakeProject';
+
+            const createProjectStub = sandbox.stub(awsWrapper.codeBuild, 'createProject').resolves({
+                project: {
+                    name: projectName
+                }
+            });
+
+            const project = await codeBuildCalls.createProject(projectName, projectName, 'pipeline',
+                'phase', 'FakeImage', { SOME_KEY: 'some_value' },
+                '777777777777', 'FakeArn', 'us-west-2', '');
+
+            expect(createProjectStub.callCount).to.equal(1);
+            expect(project!.name).to.equal(projectName);
+        });
+    });
+
+    describe('updateProject', () => {
+        it('should update the project', async () => {
+            const projectName = 'FakeProject';
+
+            const updateProjectStub = sandbox.stub(awsWrapper.codeBuild, 'updateProject').resolves({
+                project: {
+                    name: projectName
+                }
+            });
+
+            const project = await codeBuildCalls.updateProject(projectName, projectName, 'pipeline',
+                'phase', 'FakeImage', {}, '777777777777', 'FakeArn', 'us-west-2', '');
+            expect(updateProjectStub.callCount).to.equal(1);
+            expect(project!.name).to.equal(projectName);
+        });
+    });
+
+    describe('getProject', () => {
+        it('should return the project if it exists', async () => {
+            const batchGetProjectsStub = sandbox.stub(awsWrapper.codeBuild, 'batchGetProjects').resolves({
+                projects: [{}]
+            });
+
+            const project = await codeBuildCalls.getProject('FakeProject');
+            expect(batchGetProjectsStub.callCount).to.equal(1);
+            expect(project).to.deep.equal({});
+        });
+
+        it('should return null if the project doesnt exist', async () => {
+            const batchGetProjectsStub = sandbox.stub(awsWrapper.codeBuild, 'batchGetProjects').resolves({
+                projects: []
+            });
+
+            const project = await codeBuildCalls.getProject('FakeProject');
+            expect(batchGetProjectsStub.callCount).to.equal(1);
+            expect(project).to.deep.equal(null);
+        });
+    });
+
+    describe('deleteProject', () => {
+        it('should delete the project', async () => {
+            const deleteProjectStub = sandbox.stub(awsWrapper.codeBuild, 'deleteProject').resolves(true);
+
+            const success = await codeBuildCalls.deleteProject('FakeProject');
+            expect(deleteProjectStub.callCount).to.equal(1);
+            expect(success).to.equal(true);
+        });
+    });
+});
